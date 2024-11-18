f7f81bfadc9bf9027f361d44757960a3ca2142a9
Ankit choudhary <ankitrajput00015@gmail.com>
2024-11-18 22:27:35 +0530

    updated

--- src/pages/Login/SignIn.jsx
+++ src/pages/Login/SignIn.jsx
@@ -135,13 +135,14 @@ const SignIn = () => {
               backgroundImage: `url(${login})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center',
+              border:'2px solid black'
             }}
           />
         </Grid>
 
-        <Grid item xs={12} md={5} class="flex items-center justify-center p-0 ml-5">
+        <Grid item xs={12} md={5} class="flex items-center justify-center p-0 ml-2">
           <Box mt={5} px={3} display="flex" alignItems="center" height="100%">
-            <Box width="100%" maxWidth='sm'>
+            <Box width="50vw" maxWidth='sm' border='2px solid black'>
               <Typography variant="h3" align="start" gutterBottom>
                 Sign In
               </Typography>
